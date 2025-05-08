aece4967e24211290d4f651b467ac18b1d4adb2f
ontsii <oona.kotikangas@gmail.com>
2025-05-08 13:38:42 +0300

    l

---
+++ portfolio/muuttujat.js
@@ -0,0 +1,21 @@
+a = 3
+b = 5
+c = 7
+d = 15
+
+function summa() {
+    alert((a+b));
+}
+
+function vähennys() {
+    alert((b-c));
+}
+
+function kerto() {
+    alert((c*d));
+}
+
+function jako() {
+    alert((d/a));
+    
+}
