9bc7b9ae2b3b069375b3cc2aef441ab3e2ba3a44
ontsii <oona.kotikangas@gmail.com>
2025-05-09 13:12:56 +0300

    c

--- portfolio/keskustelusivu.js
+++ portfolio/keskustelusivu.js
@@ -1,1 +1,24 @@
 console.log("työkokemus sivu avattu");
+
+function send() {
+    const birthdayField = document.getElementById("syntymäpäivä");
+    const emailField = document.getElementById("sähköposti");
+    const typeField = document.getElementById("tyyppi");
+    const usageField = document.getElementById("usage");
+    const bodyField = document.getElementById("body");
+
+    const birthday = birthdayField.value;
+    const email = emailField.value;
+    const type = typeField.value;
+    const usage = usageField.value;
+    const body = bodyField.value;
+
+    console.log("Syntymäpäivä:", birthday);
+    console.log("Sähköposti:", email);
+    console.log("Tyyppi:", type);
+    console.log("Käyttö:", usage);
+    console.log("Viesti:", body);
+
+    const dialog = document.querySelector("dialog");
+    dialog.close();
+}
