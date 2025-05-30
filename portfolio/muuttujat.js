f08974ec1dc01d6eb93415f63b6b73f9b0a6da44
ontsii <oona.kotikangas@gmail.com>
2025-05-30 13:31:28 +0300

    d

--- portfolio/muuttujat.js
+++ portfolio/muuttujat.js
@@ -40,5 +40,20 @@ function laskin_jako() {
 }
 
 function paivamaara() {
-    new Date();
+    const nyt = new Date();
+
+    const paiva = nyt.getDate();
+    const kuukausi = nyt.getMonth() + 1; 
+    const vuosi = nyt.getFullYear();
+    document.getElementById("paivamaara").innerHTML = `${paiva}.${kuukausi}.${vuosi}`;
+
+    const tunti = nyt.getHours();
+    const minuutti = nyt.getMinutes();
+    const sekunti = nyt.getSeconds();
+    document.getElementById("aika").innerHTML = `${tunti}:${minuutti}:${sekunti}`;
+
+    document.getElementById("paivaJaAika").innerHTML = `${paiva}.${kuukausi}.${vuosi} klo ${tunti}:${minuutti}:${sekunti}`;
+
+    document.getElementById("viikonpaiva").innerHTML = `${nyt.toLocaleDateString('fi-FI', { weekday: 'long' })}`;
+    document.getElementById("kuukausi").innerHTML = `${nyt.toLocaleDateString('fi-FI', { month: 'long' })}`;    
 }
